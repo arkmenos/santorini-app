b59643aafef7c9afb6293a75c275f82cf87995ac
arkmenos <abbeya@gmail.com>
2024-06-28 21:23:53 -0500

    updated for deployement

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,10 @@ import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
+ 
   <Provider store={store}> 
+     <React.StrictMode>
     <App />
+  </React.StrictMode>,
   </Provider>
 )
