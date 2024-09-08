ee534b8ea2e856cf5212fd6c02c32967d5d86a07
arkmenos <abbeya@gmail.com>
2024-09-08 13:48:26 -0500

    updated server url

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 
 
-const URL = "http://localhost:4000"
+const URL = "https://santorini-server.onrender.com"
 
 export const socket = io(URL, {
     autoConnect: false, transports:['websocket', 'polling', 'webtransport'],
